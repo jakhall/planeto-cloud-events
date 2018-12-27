765d2e750fb475228526dd54997aed93a316702a
wangte <wangte@startdt.com>
2018-12-27 21:34:04 +0000

    create/modify/delete event join/quit event can work but still need improvement

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -13,6 +13,9 @@ export class AppService {
   baseUrl = 'https://planeto-app.appspot.com/api/';
   userUrl = this.baseUrl+'user/';
   public loggedIn: BehaviorSubject<boolean>;
+  eventUrl = this.baseUrl + 'event/';
+  choiceUrl = this.baseUrl + 'choice/';
+
   constructor(private http: HttpClient) {
     this.loggedIn = new BehaviorSubject<boolean>(this.hasToken());
   }
@@ -30,60 +33,37 @@ export class AppService {
   login(username:string,password:string){
     const url = this.userUrl+'login';
     let user = {
-      username:username,
-      password:password
-    }
-    return this.http.post(url,user).pipe(
-      map(res=>{
+      username: username,
+      password: password
+    };
+    return this.http.post(url, user).pipe(
+      map(res => {
         return res;
       })
     );
   }
 
   createUser(user) {
-    const url = this.userUrl+'register';
-    return this.http.post(url,user).pipe(
+    const url = this.userUrl + 'register';
+    return this.http.post(url, user).pipe(
       map(res => {
         return res;
       }));
   }
 
   getUser(userID) {
-    const uri = this.userUrl+userID; //userId will be dynamic
+    const url = this.userUrl + userID; //userId will be dynamic
 
-    return this.http.get(uri)
+    return this.http.get(url)
       .pipe(
         map(res => {
           return res;
         }));
   }
 
+  createEvent(event,userID) {
 
-
-  getEvents(userID) {
-    const uri = this.userUrl+userID+'/events'; //userId will be dynamic
-
-    return this.http.get(uri)
-      .pipe(
-        map(res => {
-          return res;
-        }));
-  }
-
-  getJoinedEvents(userID) {
-    const uri = this.userUrl+userID+'/events'; //userId will be dynamic
-
-    return this.http.get(uri)
-      .pipe(
-        map(res => {
-          return res;
-        }));
-  }
-
-  createEvent(event) {
-
-    let userID = event.userID;
-    const uri = this.userUrl+userID+'/events';
+    const uri = this.eventUrl + userID;
 
     return this.http.post(uri, event)
       .pipe(
@@ -91,34 +71,83 @@ export class AppService {
           return res;
         }));
   }
-  delEvent(){
-    return this.http.get(this.baseUrl+'test/getUser').pipe(map(res => {
-      return res;
-    }));
-  }
 
   updateEvent(event) {
-    return this.http.get(this.baseUrl+'test/getUser').pipe(map(res => {
-      return res;
-    }));
+    const url = this.eventUrl + event.eventID;
+
+    return this.http.put(url, event)
+      .pipe(
+        map(res => {
+          return res;
+        })
+      );
   }
 
-  quitEvent(choice) {
-    return this.http.get(this.baseUrl+'test/getUser').pipe(map(res => {
-      return res;
-    }));
+  deleteEvent(userID, eventID) {
+    const url = this.eventUrl + userID + '/' + eventID;
+
+    return this.http.delete(url)
+      .pipe(
+        map(res => {
+          return res;
+        })
+      );
   }
 
-  joinEvent(choice) {
-    return this.http.get(this.baseUrl+'test/getUser').pipe(map(res => {
-      return res;
-    }));
+  getEvents(userID, type) {
+    const url = this.eventUrl + type + '/' + userID; //userId will be dynamic
+
+    return this.http.get(url)
+      .pipe(
+        map(res => {
+          return <Array<any>>res;
+        }));
+  }
+
+  getEventInfo(eventId) {
+    const url = this.eventUrl + eventId; //userId will be dynamic
+
+    return this.http.get(url)
+      .pipe(
+        map(res => {
+          return String(res);
+        }));
+  }
+
+  createChoice(choice,userID) {
+    const url = this.choiceUrl+userID;
+
+    return this.http.post(url,choice)
+      .pipe(
+        map(res => {
+          return res;
+        }));
+  }
+
+  updateChoice(choice) {
+    const url = this.choiceUrl+choice.eventID;
+
+    return this.http.put(url,choice)
+      .pipe(
+        map(res => {
+          return res;
+        }));
+  }
+
+  deleteChoice(userID,eventID) {
+    const url = this.choiceUrl + userID + '/' + eventID;
+
+    return this.http.delete(url)
+      .pipe(
+        map(res => {
+          return res;
+        })
+      );
   }
 
 
-
-  debug(){
-    return this.http.get(this.baseUrl+'test/getUser').pipe(map(res => {
+  debug() {
+    return this.http.get(this.baseUrl + 'test/getUser').pipe(map(res => {
       return res;
     }));
   }
