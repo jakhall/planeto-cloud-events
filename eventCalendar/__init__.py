4c43e11b33f7265c84f5c2880389cdaeaa8ffbcb
liuyuefu <374063339@qq.com>
2018-12-27 14:22:47 +0000

    Add files via upload

---
+++ eventCalendar/__init__.py
@@ -0,0 +1,98 @@
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+from flask import current_app, Flask, redirect, request, session, url_for
+import httplib2
+# [START include]
+from oauth2client.contrib.flask_util import UserOAuth2
+import model as m
+
+oauth2 = UserOAuth2()
+# [END include]
+
+def create_app(config, debug=False, testing=False, config_overrides=None):
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    oauth2.init_app(
+        app,
+        scopes=['email', 'profile'],
+        authorize_callback =_request_user_info)
+    # [END init_app]
+
+    # [START logout]
+    # Add a logout handler.
+    @app.route('/logout')
+    def logout():
+        # Delete the user's profile and the credentials stored by oauth2.
+        del session['profile']
+        oauth2.storage.delete()
+        return redirect('/')
+    # [END logout]
+
+    # Register the Planeto CRUD blueprint.
+    from .crud import crud
+    app.register_blueprint(crud, url_prefix='/events')
+    # app.register_blueprint(crud, url_prefix='/profile')
+
+    # Add a default root route.
+    @app.route("/")
+    def index():
+        return redirect(url_for('crud.list'))
+
+    # Add an error handler. This is useful for debugging the live application,
+    # however, you should disable the output of the exception for production
+    # applications.
+    @app.errorhandler(500)
+    def server_error(e):
+        return """
+        An internal error occurred: <pre>{}</pre>
+        See logs for full stacktrace.
+        """.format(e), 500
+
+    return app
+
+# [START request_user_info]
+def _request_user_info(credentials):
+    """
+    Makes an HTTP request to the Google+ API to retrieve the user's basic
+    profile information, including full name and photo, and stores it in the
+    Flask session.
+    """
+    del session['google_oauth2_csrf_token']
+    del session['google_oauth2_credentials']
+
+    http = httplib2.Http()
+    credentials.authorize(http)
+    resp, content = http.request(
+        'https://www.googleapis.com/plus/v1/people/me')
+
+    if resp.status != 200:
+        current_app.logger.error(
+            "Error while obtaining user profile: \n%s: %s", resp, content)
+        return None
+
+    session['profile'] = json.loads(content.decode('utf-8'))
+
+    user = m.getUserById(session['profile']['id'])
+    if (not user):
+        data ={}
+        data['name'] = session['profile']['displayName']
+        data['email'] = session['profile']['emails'][0]['value']
+        data['firstname'] = session['profile']['name']['givenName']
+        data['lastname'] = session['profile']['name']['familyName']
+        user = m.createUser(data, session['profile']['id'])
+
+# [END request_user_info]
---
+++ eventCalendar/crud.py
@@ -0,0 +1,121 @@
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import model as m
+from flask import Blueprint, current_app, redirect, render_template, request, \
+    session, url_for, jsonify
+
+crud = Blueprint('crud', __name__)
+
+
+# [START list]
+@crud.route("/")
+def list():
+    token = request.args.get('page_token', None)
+    if token:
+        token = token.encode('utf-8')
+
+    events, next_page_token = m.list(cursor=token)
+
+    return render_template(
+        "list.html",
+        events=events,
+        next_page_token=next_page_token)
+
+
+# [END list]
+
+# [START list_mine]
+@crud.route("/mine")
+def list_mine():
+    token = request.args.get('page_token', None)
+    print(token)
+    if token:
+        token = token.encode('utf-8')
+
+    events, next_page_token = m.list_by_user(
+        user_id=session['profile']['id'],
+        cursor=token)
+
+    return render_template(
+        "list.html",
+        events=events,
+        next_page_token=next_page_token)
+
+
+# [END list_mine]
+
+
+@crud.route('/<id>')
+def view(id):
+    event = m.read(id)
+    return render_template("view.html", event=event)
+
+
+# [START add]
+@crud.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+
+        # If the user is logged in, associate their profile with the new event.
+        if 'profile' in session:
+            data['createdBy'] = session['profile']['displayName']
+            data['createdById'] = session['profile']['id']
+
+            event = m.create(data)
+
+            return redirect(url_for('.view', id=event['id']))
+
+    return render_template("form.html", action="Add", event={})
+
+
+# [END add]
+
+
+@crud.route('/<id>/edit', methods=['GET', 'POST'])
+def edit(id):
+    event = m.read(id)
+
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+
+        event = m.update(data, id)
+
+        return redirect(url_for('.view', id=event['id']))
+
+    return render_template("form.html", action="Edit", event=event)
+
+
+@crud.route('/<id>/delete')
+def delete(id):
+    m.delete(id)
+    return redirect(url_for('.list'))
+
+
+@crud.route('/profile', methods=['GET', 'POST'])
+def editProfile():
+    user = m.getUserById(session['profile']['id'])
+
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+
+        user = m.updateUser(data, session['profile']['id'])
+
+        return redirect('/')
+
+    return render_template("profile.html", user=user)
+
+
+
