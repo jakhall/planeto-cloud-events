b766b6b84f5dd5e794a2ef091ef0511d1759849a
Jak Hall <jakhall@hotmail.co.uk>
2019-01-03 19:59:00 +0000

    change

--- appengine_config.py
+++ appengine_config.py
@@ -2,4 +2,4 @@ from google.appengine.ext import vendor
 import os
 
 # Add any libraries installed in the "lib" folder.
-vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), '/workspace/lib'))
