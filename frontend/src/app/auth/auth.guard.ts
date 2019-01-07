28d805a5a390adc316acb4cc8f02193f04690ac9
Jak Hall <jakhall@hotmail.co.uk>
2019-01-07 10:23:08 +0000

    Update auth.guard.ts

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -16,7 +16,7 @@ export class AuthGuard implements CanActivate {
         if(data == "Exists") {
           return true;
         }
-        this.router.navigate(['launch']);
+        this.router.navigate(['login']);
         return false;
       });
 
